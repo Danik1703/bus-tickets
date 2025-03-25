747082a32cf381168ca569f88f70cd45398b98aa
Панов Даниил <kartoshkavkusnay@gmail.com>
2025-03-25 14:54:40 +0200

    email add

--- Backend/TicketBookingApi/obj/Debug/net9.0/TicketBookingApi.AssemblyInfo.cs
+++ Backend/TicketBookingApi/obj/Debug/net9.0/TicketBookingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketBookingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b12c2ce3de75e070cf437256a84a92b6fdec19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5016d31705622fd7e319bc43a6bf72f337700211")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketBookingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketBookingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
